6f330532ea2bdd1f54d4e2f7d57c0fef79a13982
mmhgu454 <71300743+mmhgu454@users.noreply.github.com>
2020-12-25 13:58:27 +0800

    Add files via upload

--- Life_Game.m
+++ Life_Game.m
@@ -1,30 +1,44 @@
 function Life_Game()
-    N=10;k=N*N/4;which=1;
-    chessboard=255*ones(N,N,3);
-    checkM=zeros(N,N);                  % 改成活的是1，改成死的是-1，沒變=0
-    % which
-    if which==1
-        lives(k,N);
-        image(chessboard);pause(2);
-    else
-        image(chessboard);pause(2);
-        disp('黑是活的白是死的，點擊的地方會活，點其他鍵結束')
-        BUTTON=1;
+% 程式用途：模擬康威生命遊戲的運作模式
+% 程式input:N、which、可能有k(這些是做什麼的在程式內有解釋)
+% 程式output:生命遊戲的figure
+%
+    N=input('畫面範圍要多大(NxN？(建議在10~100之間，比較舒服)\n');
+    which=input('希望隨機生成(下一步設定生成數)還是手動選取？(若N太大建議隨機生成)');
+    chessboard=255*ones(N,N,3);                                         %
+    checkM=zeros(N,N);                                                  % 最後判斷下一個階段的各位置細胞存活與否
+    if which==1                                                         % 0為不變，-1則變死亡，1則是復活
+        k=input(['要生成幾個點？(推薦範圍在' ...
+            num2str((1/4)*N^2) '~' num2str((1/2)*N^2) '之間)\n']);    
+        if k>=N^2;                                                      % 沒什麼用的防呆
+            disp('出事了阿伯，再重來一次ㄅ')
+            return
+        end
+        lives(k,N);                                                     % 隨機生成case的生成函數
+        image(chessboard);pause(2);                                     % 把初始圖print出來並暫停兩秒看清楚
+    elseif which==2 
+        disp('黑是活的白是死的，點擊的地方狀態會變，點其他鍵(如右鍵)結束')
+        BUTTON=1;                                                       
         while (BUTTON==1)
+            image(chessboard);                                          % 固定一個空白圖的位置給你點
             [X,Y,BUTTON]=ginput(1);
             if (BUTTON==1)
-                pick(round(Y),round(X));
-            end
-            image(chessboard)
+                pick(round(Y),round(X));                                % pick函數:改變當前點的細胞的狀態的函數
+            end         
         end
+        image(chessboard);pause(2);                                     % 把初始圖print出來並暫停兩秒看清楚
+    else
+        disp('吃錯東西了，再重來一次ㄅ')                                  % 沒什麼用的which防呆
+        return
     end
-
     change=1;
     counter=1;
-    while(change>0)                                 % 穩定態才停? 要調
-        change=0;% 怎麼判斷?
-        for ii=1:N                                  % ii、jj是每個人
-            for jj=1:N                              % kk、ww是每個人周圍八格
+    ite=0;                                          % 動態平衡時不會自動停，需要靠這個函數
+    max_ite=2000;                                   % 最多只跑二千次(約2分鐘)
+    while(change>0)                                 
+        change=0;
+        for ii=1:N                                  % ii、jj是每個細胞
+            for jj=1:N                              % kk、ww是每個細胞周圍八格
                 if chessboard(ii,jj,1)              % 死的，周圍剛好三個黑(0)活
                     times=0;                       
                     for kk=ii-1:ii+1
@@ -33,7 +47,7 @@ function Life_Game()
                         end
                     end
                     refun(times,ii,jj,false);       % 死的false
-                else
+                else                                % 活的，周圍兩個黑或三個黑則活，否則死
                     times=-1;                       % 自己活的，先扣掉
                     for kk=ii-1:ii+1
                         for ww=jj-1:jj+1
@@ -45,13 +59,17 @@ function Life_Game()
             end
         end
         % output環節
-        change=draw(checkM);
-        pause(0.05);
-        %[x,y,BUTTON]=ginput(1);    % debug 用，點了才會下一步 > <
+        change=draw(checkM);                        % draw會回傳1/0，若是0代表沒東西改變(靜態平衡，出迴圈)
+        pause(0.05);                                % 世界越快，心則慢
         image(chessboard);drawnow
-        checkM=zeros(N,N);
+        checkM=zeros(N,N);                          % 歸零儲存改變的陣列
+        if ite>=max_ite                             % 防爆
+            disp('跑了很多次，我猜已經動態平衡了');
+            return;
+        end
+        ite=ite+1;
     end
-    disp('沒了，大家都是不動的穩定態')
+    disp('沒了，大家都是不動的穩定態');
     
     % 多個內函數
     function lives(ite,N)           % 隨機挑活人的函數
